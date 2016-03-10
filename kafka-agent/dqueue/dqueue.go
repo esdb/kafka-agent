ad566a0c1fffb4885b8ac8d901d906f8ddfc7e1d
Tao Wen <taowen@gmail.com>
2016-03-10 21:20:16 +0800

    initial implementation of dqueue

--- kafka-agent/dqueue/dqueue.go
+++ kafka-agent/dqueue/dqueue.go
@@ -2,15 +2,62 @@ package dchannel
 
 import (
 	"os"
+	"encoding/binary"
 	"github.com/edsrzf/mmap-go"
+	"errors"
 )
+
+const HEADER_SIZE = 13 // 1 for version 4 for viewSize 4 for nextWriteAt 4 for nextReadAt
+const MAX_READ_PACKET_COUNT = 1024
+const MAX_PACKET_SIZE = 1024 * 16
 
 type DQueue struct {
 	fileObj *os.File
 	mappedFile mmap.MMap
+	header header
+	body []byte
+	/*
+	view covers the readable range of body
+	to avoid split packet in two parts to wrap around
+	[--- header ---][--- body ---]
+	                [--- view --]
+	 */
+	view []byte
+	readBuffers [][]byte
+	readBufferItself [][]byte
 }
 
-func Open(filePath string, nMegaBytes int) (*DQueue, error) {
+type header []byte
+
+func (header header) setVersion() {
+	header[0] = 1
+}
+
+func (header header) setViewSize(viewSize uint32) {
+	binary.BigEndian.PutUint32(header[1:], viewSize)
+}
+
+func (header header) getViewSize() uint32 {
+	return binary.BigEndian.Uint32(header[1:])
+}
+
+func (header header) setNextWriteAt(nextWriteAt uint32) {
+	binary.BigEndian.PutUint32(header[5:], nextWriteAt)
+}
+
+func (header header) getNextWriteAt() uint32 {
+	return binary.BigEndian.Uint32(header[5:])
+}
+
+func (mappedBytes header) setNextReadAt(nextReadAt uint32) {
+	binary.BigEndian.PutUint32(mappedBytes[9:], nextReadAt)
+}
+
+func (mappedBytes header) getNextReadAt() uint32 {
+	return binary.BigEndian.Uint32(mappedBytes[9:])
+}
+
+func Open(filePath string, nkiloBytes int) (*DQueue, error) {
 	fileObj, err := os.OpenFile(filePath, os.O_RDWR, 0644)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -18,8 +65,8 @@ func Open(filePath string, nMegaBytes int) (*DQueue, error) {
 			if err != nil {
 				return nil, err
 			}
-			emptyBytes := [1024 * 1024]byte{}
-			for i := 0; i < nMegaBytes; i++ {
+			emptyBytes := make([]byte, 1024)
+			for i := 0; i < nkiloBytes; i++ {
 				_, err = fileObj.Write(emptyBytes[:])
 				if err != nil {
 					return nil, err
@@ -41,9 +88,26 @@ func Open(filePath string, nMegaBytes int) (*DQueue, error) {
 	if err != nil {
 		return nil, err
 	}
+	header := header(mappedFile[:HEADER_SIZE])
+	header.setVersion()
+	header.setNextWriteAt(0)
+	header.setNextReadAt(0)
+	body := mappedFile[HEADER_SIZE:]
+	view := body[:0]
+	header.setViewSize(uint32(len(view)))
+	readBuffers := make([][]byte, MAX_READ_PACKET_COUNT)
+	readBufferItself := make([][]byte, MAX_READ_PACKET_COUNT)
+	for i := 0; i < MAX_READ_PACKET_COUNT; i++ {
+		readBuffers[i] = make([]byte, 1024 * 256)
+	}
 	return &DQueue{
 		fileObj: fileObj,
 		mappedFile: mappedFile,
+		header: header,
+		body: body,
+		view: view,
+		readBuffers: readBuffers,
+		readBufferItself: readBufferItself,
 	}, nil
 }
 
@@ -56,5 +120,70 @@ func (q *DQueue) Close() error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func (q *DQueue) Pop() ([][]byte, error) {
+	nextWriteAt := q.header.getNextWriteAt()
+	nextReadAt := q.header.getNextReadAt()
+	packetsCount := 0
+	fallBehind := false
+	if nextReadAt > nextWriteAt {
+		fallBehind = true
+	}
+	for pos := nextReadAt; packetsCount < len(q.readBufferItself); packetsCount++ {
+		if !fallBehind && pos >= nextWriteAt {
+			break
+		}
+		if pos > uint32(len(q.view)) {
+			return nil, errors.New("internal error: nextReadAt is invalid")
+		} else if pos == uint32(len(q.view)) {
+			pos = 0 // wrap around
+			fallBehind = false
+		}
+		packetSize := binary.BigEndian.Uint16(q.view[pos:])
+		if packetSize > MAX_PACKET_SIZE {
+			return nil, errors.New("packet is too large")
+		}
+		pos += 2
+		nextPos := pos + uint32(packetSize)
+		readBuffer := q.readBuffers[packetsCount][:packetSize]
+		copy(readBuffer, q.view[pos:nextPos])
+		q.readBufferItself[packetsCount] = readBuffer
+		pos = nextPos
+	}
+	return q.readBufferItself[:packetsCount], nil
+}
+
+func (q *DQueue) Push(packets [][]byte) error {
+	pos := q.header.getNextWriteAt()
+	for _, packet := range packets {
+		packetSize := uint16(len(packet))
+		if packetSize > MAX_PACKET_SIZE {
+			return errors.New("packet is too large")
+		}
+		viewSize := uint32(len(q.view))
+		willWriteTo := pos + 2 + uint32(packetSize)
+		if willWriteTo > viewSize {
+			// overflow the view
+			if pos > uint32(len(q.body)) {
+				return errors.New("internal error: nextWriteAt is invalid")
+			} else if willWriteTo > uint32(len(q.body)) {
+				// overflow the body, shrink the view
+				q.view = q.body[:pos]
+				pos = 0
+			} else {
+				// grow the view to cover
+				q.view = q.body[:willWriteTo]
+			}
+		}
+		binary.BigEndian.PutUint16(q.view[pos:pos+2], packetSize)
+		pos += 2
+		nextPos := pos + uint32(packetSize)
+		copy(q.view[pos:nextPos], packet)
+		pos = nextPos
+	}
+	q.header.setNextWriteAt(pos)
+	q.header.setViewSize(uint32(len(q.view)))
 	return nil
 }
--- kafka-agent/dqueue/dqueue_test.go
+++ kafka-agent/dqueue/dqueue_test.go
@@ -3,18 +3,130 @@ package dchannel
 import (
 	"testing"
 	"os"
+	"log"
 	"reflect"
 )
 
 func Test_empty(t *testing.T) {
+	q := openTestQueue()
+	defer q.Close()
+}
+
+func Test_pop_from_empty(t *testing.T) {
+	q := openTestQueue()
+	defer q.Close()
+	packets, _ := q.Pop()
+	assertEq(0, len(packets))
+}
+
+func Test_push_then_pop(t *testing.T) {
+	q := openTestQueue()
+	defer q.Close()
+	q.Push([][]byte{
+		[]byte("A"),
+	})
+	packets, _ := q.Pop()
+	assertEq(1, len(packets))
+	assertEq("A", string(packets[0]))
+}
+
+func Test_push_multiple_then_pop(t *testing.T) {
+	q := openTestQueue()
+	defer q.Close()
+	q.Push([][]byte{
+		[]byte("A"),
+		[]byte("B"),
+		[]byte("C"),
+	})
+	packets, _ := q.Pop()
+	assertEq(3, len(packets))
+	assertEq("A", string(packets[0]))
+	assertEq("B", string(packets[1]))
+	assertEq("C", string(packets[2]))
+}
+
+func Test_push_wrap_around_when_body_overflow(t *testing.T) {
+	q := openTestQueue()
+	defer q.Close()
+	assertEq(1011, len(q.body))
+	// A wrapped
+	q.header.setNextWriteAt(1008)
+	err := q.Push([][]byte{
+		[]byte("A"),
+	})
+	assertNil(err)
+	assertEq(uint32(1011), q.header.getNextWriteAt())
+	q.header.setNextReadAt(1008)
+	packets, _ := q.Pop()
+	assertEq(1, len(packets))
+	assertEq("A", string(packets[0]))
+	// second byte of packet size wrapped
+	q.header.setNextWriteAt(1009)
+	q.Push([][]byte{
+		[]byte("A"),
+	})
+	assertEq(uint32(3), q.header.getNextWriteAt())
+	q.header.setNextReadAt(1009)
+	packets, _ = q.Pop()
+	assertEq(1, len(packets))
+	assertEq("A", string(packets[0]))
+	// first byte of packet size wrapped
+	q.header.setNextWriteAt(1010)
+	q.Push([][]byte{
+		[]byte("A"),
+	})
+	assertEq(uint32(3), q.header.getNextWriteAt())
+	q.header.setNextReadAt(1010)
+	packets, _ = q.Pop()
+	assertEq(1, len(packets))
+	assertEq("A", string(packets[0]))
+}
+
+func Test_push_wrap_around_when_view_overflow(t *testing.T) {
+	q := openTestQueue()
+	defer q.Close()
+	assertEq(1011, len(q.body))
+	q.view = q.body[:10]
+	q.header.setNextWriteAt(10)
+	err := q.Push([][]byte{
+		[]byte("A"),
+	})
+	assertNil(err)
+	assertEq(uint32(13), q.header.getNextWriteAt())
+	q.header.setNextReadAt(10)
+	packets, _ := q.Pop()
+	assertEq(1, len(packets))
+	assertEq("A", string(packets[0]))
+}
+
+const ASSERT_FAILED = "ASERT_FAILED"
+
+func assertNotNil(obj interface{}) {
+	if obj == nil {
+		log.Println(obj, " should not be nil")
+		panic(ASSERT_FAILED)
+	}
+}
+
+func assertNil(obj interface{}) {
+	if obj != nil {
+		log.Println(obj, " should be nil")
+		panic(ASSERT_FAILED)
+	}
+}
+
+func assertEq(expected interface{}, actual interface{}) {
+	if !reflect.DeepEqual(expected, actual) {
+		log.Println(actual, " should be ", expected)
+		panic(ASSERT_FAILED)
+	}
+}
+
+func openTestQueue() *DQueue {
 	ensureFileNotExist("/tmp/q")
-	q, err := Open("/tmp/q", 1)
-	if err != nil {
-		t.Error(reflect.TypeOf(err))
-	}
-	if q != nil {
-		defer q.Close()
-	}
+	q, _ := Open("/tmp/q", 1)
+	assertNotNil(q)
+	return q
 }
 
 func ensureFileNotExist(filePath string) error {
